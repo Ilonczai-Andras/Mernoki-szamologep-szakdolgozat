29807ca8b8282bb1acbbc275b5d20bd1785feb35
Bandi1602 <ilonczai.andras16@gmail.com>
2024-07-06 16:16:29 +0200

    függvényíek közti terület ábr

--- Canvas_for_plot.py
+++ Canvas_for_plot.py
@@ -1,27 +1,36 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
-
+from turtle import color
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLabel
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from PyQt5.QtCore import Qt
 import matplotlib.pyplot as plt
+import matplotlib.style as mplstyle
+import matplotlib as mpl
 import re
 import numpy as np
 import math
 
-class Canvas(FigureCanvas):
-    text = ""
-    func = ""
+mpl.rcParams['path.simplify_threshold'] = 1.0
+mpl.rcParams['path.simplify'] = True
+mpl.rcParams['agg.path.chunksize'] = 0
+mplstyle.use('fast')
 
+class Canvas(FigureCanvas):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
-        self.fig = plt.figure(figsize=(width, height), dpi=dpi)
+        self.plotted_functions = []
+        self.fig, self.ax = plt.subplots(figsize=(width, height), dpi=dpi)
         super().__init__(self.fig)
         self.setParent(parent)
-        self.ax = self.fig.add_subplot(1, 1, 1)
-        
+
         self.interval_x = [-10, 10]  # Initial x interval
         self.interval_y = [-10, 10]  # Initial y interval
         self.zoom_factor = 0.1  # Zoom factor
         self.df = False
         self.df_func = ""
+
+        self.is_panning = False
+        self.pan_start_x = None
+        self.pan_start_y = None
 
     def f(self, x):
         return eval(self.text)
@@ -84,15 +93,20 @@ class Canvas(FigureCanvas):
         jumps = np.where(diff > threshold)[0]
         return jumps
 
-    def clear(self):
+    def clear(self, interval_x, interval_y):
         self.ax.clear()
+        self.ax.set_xlim(interval_x[0], interval_x[1])
+        self.ax.set_ylim(interval_y[0], interval_y[1])
 
     def plot_function(self, func_str, interval_x=None, interval_y=None, clear=True, C=None, df=False, df_func=""):
         if interval_x is None:
             interval_x = self.interval_x
         if interval_y is None:
             interval_y = self.interval_y
-        
+
+        self.interval_x = [interval_x[0], interval_x[1]]
+        self.interval_y = [interval_y[0], interval_y[1]]
+
         self.func = func_str.replace("A", "a")
         func_str = self.replace_numpy_funcs(func_str.replace("^", "**")).replace("A", "a")
         self.text = func_str
@@ -104,7 +118,11 @@ class Canvas(FigureCanvas):
             self.text = self.text.replace("C2", str(C[1]))
             self.text = self.text.replace("C3", str(C[2]))
 
-        x_vals = np.linspace(interval_x[0], interval_x[1], 10000)
+        x_vals = np.linspace(-1000, 1000, 10000)
+
+        if "tan" in func_str:
+            x_vals = np.linspace(-1000, 1000, 100000)
+
         y_vals = 0
 
         if func_str.isdigit():
@@ -126,15 +144,14 @@ class Canvas(FigureCanvas):
         if clear:
             self.ax.clear()
 
-        self.ax.plot(x_vals, y_vals, label=f"y = {self.func}")
+        self.ax.plot(x_vals, y_vals, color = 'black', label=f"y = {self.func}")
         self.ax.set_xlabel("x")
         self.ax.set_xlim(interval_x[0], interval_x[1])
         self.ax.set_ylim(interval_y[0], interval_y[1])
         self.ax.set_ylabel("f(x)")
         self.ax.grid(True)
-        self.ax.legend()
-        self.fig.savefig("plot.png")
-
+        self.ax.legend(loc='lower right')
+        self.fig.canvas.draw()
         if df:
             self.direction_field(df_func, clear=False)
 
@@ -163,21 +180,99 @@ class Canvas(FigureCanvas):
         self.ax.set_ylim(self.interval_y[0], self.interval_y[1])
         self.ax.set_xlim(self.interval_x[0], self.interval_x[1])
         self.ax.set_title("Íránymező " + text)
-        self.ax.legend()
+        self.ax.legend(loc='lower right')
         self.fig.canvas.draw()
-        self.draw_idle()
+
+    def store_function(self, func_str, interval_x, interval_y, C=None, df=False, df_func=""):
+        # Check if the function is already in the list
+        for existing_func in self.plotted_functions:
+            if existing_func[0] == func_str:
+                print(f"Function {func_str} is already plotted.")
+                return
+
+        self.plotted_functions.append((func_str, interval_x, interval_y, C, df, df_func))
 
     def wheelEvent(self, event):
-        
         if event.angleDelta().y() > 0:  # Scroll up
-            self.interval_x[0] /= (1 + self.zoom_factor)
-            self.interval_x[1] /= (1 + self.zoom_factor)
-            self.interval_y[0] /= (1 + self.zoom_factor)
-            self.interval_y[1] /= (1 + self.zoom_factor)
+            self.interval_x = [self.interval_x[0] * (1 + self.zoom_factor), self.interval_x[1] * (1 + self.zoom_factor)]
+            self.interval_y = [self.interval_y[0] * (1 + self.zoom_factor), self.interval_y[1] * (1 + self.zoom_factor)]
         else:  # Scroll down
-            self.interval_x[0] *= (1 + self.zoom_factor)
-            self.interval_x[1] *= (1 + self.zoom_factor)
-            self.interval_y[0] *= (1 + self.zoom_factor)
-            self.interval_y[1] *= (1 + self.zoom_factor)
+            self.interval_x = [self.interval_x[0] / (1 + self.zoom_factor), self.interval_x[1] / (1 + self.zoom_factor)]
+            self.interval_y = [self.interval_y[0] / (1 + self.zoom_factor), self.interval_y[1] / (1 + self.zoom_factor)]
 
-        self.plot_function(self.func, self.interval_x, self.interval_y, C=[1,1,1], df=self.df, df_func=self.df_func)
+        self.ax.set_xlim(self.interval_x)
+        self.ax.set_ylim(self.interval_y)
+        self.fig.canvas.draw_idle()
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.is_panning = True
+            self.pan_start_x = event.x()
+            self.pan_start_y = event.y()
+
+    def mouseMoveEvent(self, event):
+        if self.is_panning:
+            dx = (event.x() - self.pan_start_x) / self.width()
+            dy = (event.y() - self.pan_start_y) / self.height()
+
+            x_range = self.interval_x[1] - self.interval_x[0]
+            y_range = self.interval_y[1] - self.interval_y[0]
+
+            self.interval_x[0] -= dx * x_range
+            self.interval_x[1] -= dx * x_range
+            self.interval_y[0] += dy * y_range
+            self.interval_y[1] += dy * y_range
+
+            self.ax.set_xlim(self.interval_x)
+            self.ax.set_ylim(self.interval_y)
+            self.fig.canvas.draw_idle()
+
+            self.pan_start_x = event.x()
+            self.pan_start_y = event.y()
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.is_panning = False
+
+    def plot_area_between_functions(self, x_intervals, inequality_type):
+        if len(self.plotted_functions) < 2:
+            print("Not enough functions to plot the area between them.")
+            return
+
+        func1_str, interval_x1, interval_y1, _, _, _ = self.plotted_functions[0]
+        func2_str, interval_x2, interval_y2, _, _, _ = self.plotted_functions[1]
+
+        func1 = self.replace_numpy_funcs(func1_str.replace("^", "**")).replace("A", "a")
+        func2 = self.replace_numpy_funcs(func2_str.replace("^", "**")).replace("A", "a")
+
+        print(f"Plotting area with intervals: {x_intervals}")  # Debug statement
+
+        # Ensure x_intervals has pairs of intervals
+        if len(x_intervals) % 2 != 0:
+            print(f"Invalid number of intervals: {x_intervals}")  # Debug statement
+            return
+
+        x_vals = np.linspace(-100, 100, 1000)
+        y_vals_func1 = np.array([eval(func1) for x in x_vals])
+        y_vals_func2 = np.array([eval(func2) for x in x_vals])
+
+        for i in range(0, len(x_intervals), 2):
+            start = x_intervals[i]
+            end = x_intervals[i+1]
+            mask = (x_vals >= start) & (x_vals <= end)
+
+            if inequality_type == "<":
+                mask = mask & (y_vals_func1 < y_vals_func2)
+            elif inequality_type == "<=":
+                mask = mask & (y_vals_func1 <= y_vals_func2)
+            elif inequality_type == ">":
+                mask = mask & (y_vals_func1 > y_vals_func2)
+            elif inequality_type == ">=":
+                mask = mask & (y_vals_func1 >= y_vals_func2)
+            
+            self.ax.fill_between(x_vals, y_vals_func1, y_vals_func2, where=mask, color='red', alpha=0.5, label=f"Area between {func1_str} and {func2_str}")
+
+        self.ax.legend(loc='lower right')
+        self.fig.canvas.draw()
+
+
