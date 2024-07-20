9032a98a46d7f0ceb1db0881b01bc55c487ae9bb
Bandi1602 <ilonczai.andras16@gmail.com>
2024-07-20 22:01:33 +0200

    hipotézis vizsgálat t test

--- t.py
+++ t.py
@@ -1,23 +1,53 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import numpy as np  
+from scipy import stats
+import statistics
+from sympy import sqrt, N
 
-def direction_field(f):
+def t_test(self, mu=None, alpha=None, x=None, y=None):
 
-    nt, nv = 0.5, 0.5
-    t = np.arange(-4, 4, nt)
-    v = np.arange(-4, 4, nv)
+    if x is not None and mu is not None and alpha is not None:
+        result_string = ""
 
-    # rectangular grid with points
-    x, t = np.meshgrid(t, v)
+        avg = statistics.mean(x)
+        stan_dev = statistics.stdev(x)
+        m = mu
+        n = len(x)
 
-    # derivative
-    func = f.split("=")[1]
-    print(func)
-    dv = eval(func)
-    dt = np.ones(dv.shape)
+        Alpha = 1 - alpha
+        df = n - 1
 
-    plt.quiver(x, t, dt, dv, color='black')
-    plt.title('Direction Field for ' + func)
-    plt.show()
 
-direction_field("x'(t)=np.sin(t)")
+        t = N( (avg - m) / (stan_dev / sqrt(n)) )
+
+        t_p = stats.t.ppf(q= Alpha/2,df= df)
+        print(f"df: {df} t: {t}")
+        p_value = 2 * (1 - stats.t.sf(abs(float(t)), df))
+
+        if abs(t) >= t_p:
+            
+            result_string += f"\nA nullhipotézist elvetjük t: {t} p: {2 - p_value}\n"
+            result_string += f"two tail  p:{stats.t.ppf(q= Alpha/2,df= df)}" +"\n"
+            result_string += f"one tail  p:{stats.t.ppf(q= Alpha,df= df)}" +"\n"
+        elif abs(t) < t_p:
+            result_string += f"\nA nullhipotézist elfogadjuk t: {t} p: {2 - p_value}\n"
+            result_string += f"two tail  p:{stats.t.ppf(q= Alpha/2,df= df)}" +"\n"
+            result_string += f"one tail  p:{stats.t.ppf(q= Alpha,df= df)}" +"\n"
+
+        print(result_string)
+    if (x is not None and y is not None and alpha is not None):
+
+        # Perform two-sample t-test
+        t_statistic, p_value = stats.ttest_ind(x, y)
+
+        Alpha = 1 - alpha
+        if p_value < Alpha:
+            print("H0-t elvetjük")
+        else:
+            print("H0-t elfogadjuk")
+
+        print(f"P: {p_value} T: {t_statistic}")
+
+x = [41, 34, 33, 36, 40, 25, 31, 37, 34, 30, 38]
+y = [52, 57, 62, 55, 64, 57, 56, 55]
+
+t_test(mu=40,alpha=0.95, x=[41, 34, 33, 36, 40, 25, 31, 37, 34, 30, 38])
