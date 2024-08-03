c6f1782f7614e5274542ed88815aa8b7131a29e1
Bandi1602 <ilonczai.andras16@gmail.com>
2024-08-03 17:28:16 +0200

    version 2

--- Engineering calculator/setup.py
+++ Engineering calculator/setup.py
@@ -1,11 +1,14 @@
 from cx_Freeze import setup, Executable
 import sys
-import sys
+import os
+
 # Increase the recursion limit
 sys.setrecursionlimit(1500)
 
-# Dependencies are automatically detected, but it might need
-# fine tuning.
+# Determine the absolute path to the icon
+icon_path = os.path.join(os.path.dirname(__file__), 'Mernoki-szamologep-szakdolgozat', 'Engineering calculator', 'icon.ico')
+
+# Dependencies are automatically detected, but it might need fine tuning.
 build_options = {'packages': [], 'excludes': []}
 
 base = 'Win32GUI' if sys.platform == 'win32' else None
@@ -13,11 +16,11 @@ base = 'Win32GUI' if sys.platform == 'win32' else None
 executables = [
     Executable('Main.py', base=base, 
                target_name='Engineering Calculator', 
-               icon='C:/Users/kille/OneDrive/Mernoki-szamologep-szakdolgozat/Engineering calculator/icon.ico')
+               icon=icon_path)
 ]
 
 setup(name='Engineering calc',
-      version = '1',
-      description = 'Engineering calc for engineering students',
-      options = {'build_exe': build_options},
-      executables = executables)
+      version='1',
+      description='Engineering calc for engineering students',
+      options={'build_exe': build_options},
+      executables=executables)
