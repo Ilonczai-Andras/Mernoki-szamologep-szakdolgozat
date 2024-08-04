33139e26281dee5d33458a84270831d042c26cc9
Bandi1602 <ilonczai.andras16@gmail.com>
2024-08-04 21:40:28 +0200

    installer upgrade

--- Engineering calculator/installer.bat
+++ Engineering calculator/installer.bat
@@ -2,19 +2,37 @@
 REM Navigate to the directory containing setup.py
 cd /d "%~dp0"
 
-REM Check if pip is installed
-python -m ensurepip --default-pip
+REM Function to check if pip is installed and install packages
+:install_packages
+    %1 -m ensurepip --default-pip
+    %1 -m pip install cx_Freeze==7.2.0 PyQt5==5.15.10 sympy==1.12 numpy==1.26.4 scipy==1.14.0 matplotlib==3.8.3
+    exit /b
 
-REM Install necessary packages using pip
-python -m pip install cx_Freeze PyQt5 sympy numpy scipy matplotlib
+REM Check if python is installed
+where python >nul 2>&1
+if %errorlevel% equ 0 (
+    call :install_packages python
+    python setup.py build
+    goto :end
+)
 
-REM Run the Python setup script with the build command
-python setup.py build
+REM Check if py is installed
+where py >nul 2>&1
+if %errorlevel% equ 0 (
+    call :install_packages py
+    py setup.py build
+    goto :end
+)
 
+REM If neither python nor py is found, display an error message
+echo Neither python nor py command is available.
+exit /b 1
+
+:end
 REM Optionally, check if the script executed successfully
 if %errorlevel% equ 0 (
     echo Python setup script executed successfully.
 ) else (
     echo Python setup script failed to execute.
 )
-pause 
+pause
